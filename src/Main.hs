93f1411de19655029fbd200dd5cc26ecee728f55
reckbo <reckbo@bwh.harvard.edu>
2016-08-31 16:37:32 +1000

    STY: Moved eddy preprocesing to Preproc

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,10 @@
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE OverloadedStrings    #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# OPTIONS_GHC -fno-warn-type-defaults #-}
-{-# LANGUAGE DeriveGeneric        #-}
-{-# LANGUAGE RecordWildCards      #-}
-
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as B
 import           Data.Csv
-import           Data.Function
-import           Data.List
 import           Development.Shake
 import           FSL
-import           GHC.Generics
+import           Preproc
 
 pas :: [FilePath]
 pas = [
@@ -27,107 +17,6 @@ aps = [
   "BIO_0001.dwiAP1.nii.gz",
   "BIO_0001.dwiAP2.nii.gz"
   ]
-
-data PhaseDirection = RL | PA
-
--- phaseDirection = PA
-
-b0maxbval :: BValue
-b0maxbval = BValue 50
-
-b0dist :: Int
-b0dist = 45
-
--- phaseLength :: FilePath -> Action PhaseLength
--- phaseLength dwi = case phaseDirection of
---   PA -> read . fromStdout <$> command [] "fslval" [dwi, "dim1"]
---   _   -> read . fromStdout <$> command [] "fslval" [dwi, "dim2"]
-
-type EchoSpacing = Float
-type PhaseLength = Int
-
-readoutTime :: PhaseLength -> EchoSpacing -> Float
-readoutTime l echo = (echo * numPEsteps) / 1000
-  where numPEsteps = fromIntegral $ l - 1
-
-type DWI = FilePath
-
-acqParamsPos :: String
-acqParamsPos = "0 1 0 0.8"
-acqParamsNeg = "0 -1 0 0.8"
-
-getValidB0Indices :: [BValue] -> [Int]
-getValidB0Indices bs = reverse $ foldl' f [i0] indices
-  where
-    f (i:is) i' = if (i' - i) >= b0dist
-                     then i':i:is
-                     else i:is
-    f _ _ = error "getValideB0Indices: DWI must have at least two b-values."
-    (i0:indices) = findIndices (< b0maxbval) bs
-
-mkDWIInfo :: Int -> DWI -> [BValue] -> Int -> DWIInfo
-mkDWIInfo pid dwi bs matchingLength
-  = DWIInfo
-  pid
-  dwi
-  (length bs)
-  (findIndices (< b0maxbval) bs)
-  (getValidB0Indices bs)
-  (filter (< matchingLength) $ getValidB0Indices bs)
-
-mkDWIPair :: Int -> DWI -> DWI -> [BValue] -> [BValue] -> DWIPair
-mkDWIPair pid dwi dwi' bs bs'
-  = DWIPair
-  (mkDWIInfo pid dwi bs matchingLength)
-  (mkDWIInfo pid dwi' bs' matchingLength)
-  where matchingLength = (min`on`length) bs bs'
-
-data DWIInfo = DWIInfo
-    {
-     pairId               :: Int
-    ,dwi                  :: FilePath
-    ,size                 :: Int
-    ,b0indices            :: [Int]
-    ,b0indicesWithMinDist :: [Int]
-    ,b0indicesToUse       :: [Int]
-    }
-  deriving (Show, Generic)
-
-instance ToField [Int] where
-  toField = toField . unwords . map show
-instance ToNamedRecord DWIInfo
-instance DefaultOrdered DWIInfo
-
-data DWIPair = DWIPair { pos :: DWIInfo, neg :: DWIInfo }
-  deriving Show
-
-writeB0s :: FilePath -> [DWIInfo] -> Action ()
-writeB0s out dwiinfos =
-  do fs <- traverse writeB0 dwiinfos
-     mergeVols out fs
-  where
-    writeB0 dwiinfo = extractVols (dwi dwiinfo) (b0indicesToUse dwiinfo)
-
-numValidB0s :: (DWIPair -> DWIInfo) -> [DWIPair] -> Int
-numValidB0s posneg xs = sum $  map (length . b0indicesToUse . posneg) xs
-
-writeAcqparms :: FilePath -> [DWIPair] -> Action ()
-writeAcqparms out dwipairs =
-  writeFile' out $ unlines (acq ++ acq')
-  where
-    acq = replicate (numValidB0s pos dwipairs) acqParamsPos
-    acq' = replicate (numValidB0s neg dwipairs) acqParamsNeg
-
-writeIndex :: FilePath -> [DWIPair] -> Action ()
-writeIndex out dwipairs = writeFile' out (unlines $ indexPos ++ indexNeg)
-  where
-    numVols posneg = sum . map (size . posneg)
-    indexPos = replicate (numVols pos dwipairs) "0"
-    indexNeg = replicate (numVols neg dwipairs) (show $ 1 +  numValidB0s pos dwipairs)
-
-writeCombined :: FilePath -> [DWIPair] -> Action ()
-writeCombined out dwipairs
-  = mergeVols out $ map (dwi . pos) dwipairs ++ map (dwi . neg) dwipairs
 
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="build", shakeVerbosity=Chatty} $ do
@@ -158,4 +47,4 @@ main = shakeArgs shakeOptions{shakeFiles="build", shakeVerbosity=Chatty} $ do
       writeB0s outb0s $ (map pos dwiPairs) ++ map neg dwiPairs
       writeCombined outvol dwiPairs
       writeIndex outindex dwiPairs
-      writeAcqparms acqparams dwiPairs
+      writeAcqparams acqparams dwiPairs
---
+++ src/Preproc.hs
@@ -0,0 +1,133 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveGeneric        #-}
+module Preproc
+  (
+    mkDWIPair
+    , phaseDirection (..)
+    , writeB0s
+    , writeCombined
+    , writeIndex
+    , writeAcqparams
+    , DWIPair (..)
+
+  ) where
+
+import           Data.Csv
+import           Data.Function
+import           Data.List
+import           Development.Shake
+import           FSL
+import           GHC.Generics
+
+type EchoSpacing = Float
+type PhaseLength = Int
+type DWI = FilePath
+
+b0maxbval :: BValue
+b0maxbval = BValue 50
+
+b0dist :: Int
+b0dist = 45
+
+acqParamsPAPos :: String
+acqParamsPAPos = "0 1 0"
+acqParamsPANeg :: String
+acqParamsPANeg = "0 -1 0"
+acqParamsRLPos :: String
+acqParamsRLPos = "1 0 0"
+acqParamsRLNeg :: String
+acqParamsRLNeg = "-1 0 0"
+
+data PhaseDirection = RL | PA
+
+readPhaseLength :: PhaseDirection -> FilePath -> Action PhaseLength
+readPhaseLength pedir dwi = case pedir of
+  PA -> read . fromStdout <$> command [] "fslval" [dwi, "dim1"]
+  _   -> read . fromStdout <$> command [] "fslval" [dwi, "dim2"]
+
+readoutTime :: PhaseLength -> EchoSpacing -> Float
+readoutTime l echo = (echo * numPEsteps) / 1000
+  where numPEsteps = fromIntegral $ l - 1
+
+getValidB0Indices :: [BValue] -> [Int]
+getValidB0Indices bs = reverse $ foldl' f [i0] indices
+  where
+    f (i:is) i' = if (i' - i) >= b0dist
+                     then i':i:is
+                     else i:is
+    f _ _ = error "getValideB0Indices: DWI must have at least two b-values."
+    (i0:indices) = findIndices (< b0maxbval) bs
+
+mkDWIInfo :: Int -> DWI -> [BValue] -> Int -> DWIInfo
+mkDWIInfo pid dwi bs matchingLength
+  = DWIInfo
+  pid
+  dwi
+  (length bs)
+  (findIndices (< b0maxbval) bs)
+  (getValidB0Indices bs)
+  (filter (< matchingLength) $ getValidB0Indices bs)
+
+mkDWIPair :: Int -> DWI -> DWI -> [BValue] -> [BValue] -> DWIPair
+mkDWIPair pid dwi dwi' bs bs'
+  = DWIPair
+  (mkDWIInfo pid dwi bs matchingLength)
+  (mkDWIInfo pid dwi' bs' matchingLength)
+  where matchingLength = (min`on`length) bs bs'
+
+data DWIInfo = DWIInfo
+    {
+     _pairId               :: Int
+    ,_dwi                  :: FilePath
+    ,_size                 :: Int
+    ,_b0indices            :: [Int]
+    ,_b0indicesWithMinDist :: [Int]
+    ,_b0indicesToUse       :: [Int]
+    }
+  deriving (Show, Generic)
+
+instance ToField [Int] where
+  toField = toField . unwords . map show
+instance ToNamedRecord DWIInfo
+instance DefaultOrdered DWIInfo
+
+data DWIPair = DWIPair { pos :: DWIInfo, neg :: DWIInfo }
+  deriving Show
+
+writeB0s :: FilePath -> [DWIInfo] -> Action ()
+writeB0s out dwiinfos =
+  do fs <- traverse writeB0 dwiinfos
+     mergeVols out fs
+  where
+    writeB0 dwiinfo = extractVols (_dwi dwiinfo) (_b0indicesToUse dwiinfo)
+
+numValidB0s :: (DWIPair -> DWIInfo) -> [DWIPair] -> Int
+numValidB0s posneg xs = sum $  map (length . _b0indicesToUse . posneg) xs
+
+writeAcqparams :: FilePath -> PhaseDirection -> EchoSpacing -> [DWIPair] -> Action ()
+writeAcqparams out pedir echo dwipairs = do
+  phase <- readPhaseLength (pos . head . dwipairs)
+  let readout = show $ readoutTime phase echo
+  writeFile' out $ unlines (acq ++ acq')
+  where
+    (p, n) = (numValidB0s pos dwipairs, numValidB0s neg dwipairs)
+    acqParamsPos =  case pedir of
+      PA -> "0 1 0 " ++ readout
+      RL -> "1 0 0 " ++ readout
+    acqParamsNeg = case pedir of
+      PA -> "0 -1 0 " ++ readout
+      RL -> "-1 0 0 " ++ readout
+    acq = replicate p acqParamsPos
+    acq' = replicate n acqParamsNeg
+
+writeIndex :: FilePath -> [DWIPair] -> Action ()
+writeIndex out dwipairs = writeFile' out (unlines $ indexPos ++ indexNeg)
+  where
+    numVols posneg = sum . map (_size . posneg)
+    indexPos = replicate (numVols pos dwipairs) "0"
+    indexNeg = replicate (numVols neg dwipairs) (show $ 1 +  numValidB0s pos dwipairs)
+
+writeCombined :: FilePath -> [DWIPair] -> Action ()
+writeCombined out dwipairs = do
+  mergeVols out $ map (_dwi . pos) dwipairs ++ map (_dwi . neg) dwipairs
+  trimVol out
